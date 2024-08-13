3bc9ea2116009f2d19f762d070d18e0eebc9bd2d
abhishekx1212 <trigonx1212@gmail.com>
2024-08-13 10:42:41 +0530

    first commit

---
+++ createServer/index.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const fs = require('fs');
+
+const port = 8081;
+
+const server = http.createServer((req, res) => {
+    let fileName = "";
+
+    switch (req.url) {
+        case "/":
+            fileName = "index.html"
+            break;
+        case "/about":
+            fileName = "about.html"
+            break;
+        case "/contact":
+            fileName = "contact.html"
+            break;
+    }
+
+    fs.readFile(fileName, (err, result) => {
+        if (!err) {
+            res.end(result);
+        }
+    })
+
+    // fs.appendFile('temp.txt','t',(err,result)=>{
+    //     fs.readFile('temp.txt', (err, result) => {
+    //         if (!err) {
+    //             res.end(result);
+    //         }
+    //     })
+    // })
+})
+
+server.listen(port, (error) => {
+    if (error) {
+        console.log('server not started.')
+        return false;
+    }
+    console.log(`server started http://localhost:${port}`)
+});
